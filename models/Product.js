f61fe4aef1226669194aefacb1d51efbfc0f366f
Finn Stitcher <foundfootage@protonmail.com>
2022-08-01 23:12:29 -0700

    models defined

--- models/Product.js
+++ models/Product.js
@@ -8,7 +8,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    product_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    price: {
+        type: DataTypes.DECIMAL,
+        allowNull: false,
+        validate: {
+            isDecimal: true
+        }
+    },
+    stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        default: 10,
+        validate: {
+            isInt: true
+        }
+    },
+    category_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'category',
+            key: 'id'
+        }
+    }
   },
   {
     sequelize,
